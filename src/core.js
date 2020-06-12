8ab9f90244cffc1aa4c6145ee0f094452d32312a
yunti <yunti9@icloud.com>
2020-06-12 16:08:47 +0100

    Change to element object

--- src/core.js
+++ src/core.js
@@ -1,6 +1,11 @@
 'use strict'
 
-const element = <h1 title="foo">Hello</h1>
-const element2 = React.createElement('h1', { title: 'foo' }, 'Hello')
+const element = {
+  type: 'h1',
+  props: {
+    title: 'foo',
+    children: 'Hello',
+  },
+}
 const container = document.getElementById('root')
 ReactDOM.render(element, container)
