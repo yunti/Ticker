179c15a2ecbb3f80f6d2015d8bfdbc31bdcf7d0f
yunti <yunti9@icloud.com>
2020-06-12 20:21:56 +0100

    Restart from scratch without deleting, rendering h1

--- src/index.js
+++ src/index.js
@@ -1,72 +1,77 @@
 'use strict'
 
-function createElement(type, props, ...children) {
-  return {
-    type,
-    props: {
-      ...props,
-      children: children.map((child) =>
-        typeof child === 'object' ? child : createTextElement(child),
-      ),
-    },
-  }
-}
+// function createElement(type, props, ...children) {
+//   return {
+//     type,
+//     props: {
+//       ...props,
+//       children: children.map((child) =>
+//         typeof child === 'object' ? child : createTextElement(child),
+//       ),
+//     },
+//   }
+// }
 
-function createTextElement(text) {
-  return {
-    type: 'TEXT_ELEMENT',
-    props: {
-      nodeValue: text,
-      children: [],
-    },
-  }
-}
+// function createTextElement(text) {
+//   return {
+//     type: 'TEXT_ELEMENT',
+//     props: {
+//       nodeValue: text,
+//       children: [],
+//     },
+//   }
+// }
 
-function render(element, container) {
-  const dom =
-    element.type == 'TEXT_ELEMENT'
-      ? document.createTextNode('')
-      : document.createElement(element.type)
+// function render(element, container) {
+//   const dom =
+//     element.type == 'TEXT_ELEMENT'
+//       ? document.createTextNode('')
+//       : document.createElement(element.type)
 
-  const isProperty = (key) => key !== 'children'
-  Object.keys(element.props)
-    .filter(isProperty)
-    .forEach((name) => {
-      dom[name] = element.props[name]
-    })
+//   const isProperty = (key) => key !== 'children'
+//   Object.keys(element.props)
+//     .filter(isProperty)
+//     .forEach((name) => {
+//       dom[name] = element.props[name]
+//     })
 
-  element.props.children.forEach((child) => render(child, dom))
-  container.appendChild(dom)
-}
+//   element.props.children.forEach((child) => render(child, dom))
+//   container.appendChild(dom)
+// }
 
-const Ticker = {
-  createElement,
-  render,
-}
+// const Ticker = {
+//   createElement,
+//   render,
+// }
 
-// const element = Ticker.createElement(
-// 'div',
-// { id: 'foot' },
-// Ticker.createElement('a', null, 'bar'),
-// Ticker.createElement('b'),
+// const element = (
+//   <div id="foo" style="background: salmon">
+//     <h1>Hello World</h1>
+//     <a href="">bar</a>
+//     <b></b>
+//   </div>
 // )
 
-const element = (
-  <div id="foo" style="background: salmon">
-    <h1>Hello World</h1>
-    <a href="">bar</a>
-    <b></b>
-  </div>
-)
-
 const container = document.getElementById('root')
-// Ticker.render(element, container)
-const node = document.createElement({
-  type: 'h1',
-  props: {
-    children: 'Hello',
-  },
-})
 
+const node = document.createElement('h1')
 const text = document.createTextNode('')
-text['nodeValue'] = element.props.children
+text['nodeValue'] = 'Hello'
+
+// const element = {
+//   type: 'h1',
+//   props: {
+//     title: 'foo',
+//     children: 'Hello',
+//   },
+// }
+
+// const node = document.createElement(element.type)
+// node['title'] = element.props.title
+
+// const text = document.createTextNode('')
+// text['nodeValue'] = element.props.children
+
+node.appendChild(text)
+container.appendChild(node)
+// Ticker.render(element, container)
