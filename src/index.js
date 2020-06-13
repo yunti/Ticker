6c93fe8270301ab5d38c61471b35d8117719a168
yunti <yunti9@icloud.com>
2020-06-13 21:37:50 +0100

    Add basic createElement function

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
 'use strict'
 
-// function createElement(type, props, ...children) {
-//   return {
-//     type,
-//     props: {
-//       ...props,
-//       children: children.map((child) =>
-//         typeof child === 'object' ? child : createTextElement(child),
-//       ),
-//     },
-//   }
-// }
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      // children: children.map((child) =>
+      // typeof child === 'object' ? child : createTextElement(child),
+      // ),
+      // children: createTextElement(children),
+      children: children[0],
+    },
+  }
+}
 
-// function createTextElement(text) {
-//   return {
-//     type: 'TEXT_ELEMENT',
-//     props: {
-//       nodeValue: text,
-//       children: [],
-//     },
-//   }
-// }
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  }
+}
 
 // function render(element, container) {
 //   const dom =
@@ -39,11 +41,22 @@
 //   container.appendChild(dom)
 // }
 
-// const Ticker = {
-//   createElement,
-//   render,
-// }
+const Ticker = {
+  createElement,
+  // render,
+}
+/** @jsx Ticker.createElement */
+// const element = <h1 title="foo">Hello World</h1>
 
+const element = Ticker.createElement(
+  'h1',
+  {
+    title: 'foo',
+  },
+  'Hello World',
+)
+
+/** @jsx Ticker.createElement */
 // const element = (
 //   <div id="foo" style="background: salmon">
 //     <h1>Hello World</h1>
@@ -51,20 +64,20 @@
 //     <b></b>
 //   </div>
 // )
-
+console.log(element)
 const container = document.getElementById('root')
 
 // const node = document.createElement('h1')
 // const text = document.createTextNode('')
 // text['nodeValue'] = 'Hello'
 
-const element = {
-  type: 'h1',
-  props: {
-    title: 'foo',
-    children: 'Hello World',
-  },
-}
+// const element = {
+//   type: 'h1',
+//   props: {
+//     title: 'foo',
+//     children: 'Hello World',
+//   },
+// }
 
 const node = document.createElement(element.type)
 node['title'] = element.props.title
