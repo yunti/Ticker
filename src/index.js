4eb4ae1f34f01f8423ea01e20c7e2e56ee5bea63
yunti <yunti9@icloud.com>
2020-06-25 23:07:26 +0100

    Add dom reuse to reconcilliation

--- src/index.js
+++ src/index.js
@@ -32,14 +32,37 @@ function render(element, container) {
 
 function reconcile(parentDom, instance, element) {
   if (instance == null) {
+    // Create instance
     const newInstance = instantiate(element)
     parentDom.appendChild(newInstance.dom)
     return newInstance
+  } else if (instance.element.type === element.type) {
+    // Update instance
+    updateDomProperties(instance.dom, instance.element.props, element.props)
+    instance.childInstances = reconcileChildren(instance, element)
+    instance.element = element
+    return instance
   } else {
+    // Replace instance
     const newInstance = instantiate(element)
     parentDom.replaceChild(newInstance.dom, instance.dom)
     return newInstance
   }
+}
+
+function reconcileChildren(instance, element) {
+  const dom = instance.dom
+  const childInstances = instance.childInstances
+  const nextChildElements = element.props.children || []
+  const newChildInstances = []
+  const count = Math.max(childInstances.length, nextChildElements.length)
+  for (let i = 0; i < count; i++) {
+    const childInstance = childInstances[i]
+    const childElement = nextChildElements[i]
+    const newChildInstance = reconcile(dom, childInstance, childElement)
+    newChildInstance.push(newChildInstance)
+  }
+  return newChildInstances
 }
 
 function instantiate(element, parentDom) {
@@ -67,7 +90,7 @@ function updateDomProperties(dom, prevProps, nextProps) {
   const isEvent = (key) => key.startsWith('on')
   const isProperty = (key) => !isEvent(key) && key !== 'children'
 
-  // Remove event listners
+  // Remove event listeners
   Object.keys(prevProps)
     .filter(isEvent)
     .forEach((key) => {
@@ -124,4 +147,4 @@ function tick() {
 
 tick()
 setInterval(tick, 1000)
-Ticker.render(element, container)
+// Ticker.render(element, container)
